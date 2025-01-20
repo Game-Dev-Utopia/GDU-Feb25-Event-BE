741f14ae80767d3359dc88b169f4b4df61d61d42
Mayuri Kolhe <77161474+MayuriKolhe-2003@users.noreply.github.com>
2025-01-20 11:16:48 +0530

    added more fields in event model

--- src/model/events.model.js
+++ src/model/events.model.js
@@ -1,31 +1,42 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 
 const eventSchema = new Schema({
-    name : {
-        type : String,
-        required : true,
-        index : true
+    name: {
+        type: String,
+        required: true,
+        index: true
     },
-    description : {
-        type : String,
+    description: {
+        type: String,
     },
-    date : {
-         type : Date,
-         required : true
+    date: {
+        type: Date,
+        required: true
     },
-    typeOfevent : {
-        type : String,
-        required : true
+    typeOfevent: {
+        type: String,
+        required: true
     },
-    registrationFee : {
-        type : Number,
-        required : true
+    registrationFee: {
+        type: Number,
+        required: true
     },
-    teamSize : {
-        type : Number,
+    teamSize: {
+        type: Number,
+    },
+    venue: {
+        type: String,
+    },
+    rule: [
+        {
+            type: String,
+        }
+    ],
+    imageUrl:{
+        type: String,
     }
-}, {timestamps : true})
+}, { timestamps: true })
 
 
 export const Event = mongoose.model("Event", eventSchema)
