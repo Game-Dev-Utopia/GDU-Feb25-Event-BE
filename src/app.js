1cbf09c25adcc2c2fddf1bc88ee923ccc7fe7e74
Mayuri Kolhe <77161474+MayuriKolhe-2003@users.noreply.github.com>
2025-01-29 11:49:40 +0530

    updated origin link

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ const app = express()
 app.use(limiter)  
 app.use(speedLimiter)
 app.use(cors({
-    origin: "http://localhost:3000", // ✅ Must match exactly with frontend
+    // origin: "http://localhost:3000", // ✅ Must match exactly with frontend
+    origin : "https://glitched.gamedevutopia.in",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // ✅ Ensure all required methods
     allowedHeaders: ["Content-Type", "Authorization"], // ✅ Explicitly allow headers
     credentials: true // ✅ Required for cookies
@@ -30,7 +31,8 @@ app.use(cors({
 
 // ✅ Manually handle preflight (`OPTIONS`) requests
 app.options("*", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://glitched.gamedevutopia.in");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
